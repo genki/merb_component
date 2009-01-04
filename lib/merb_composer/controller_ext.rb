c80640d8692efa6d0f8f251acfc1d2a2f821ea19
takiuchi <genki@s21g.com>
2009-01-05 05:32:41 +0900

    Added gemspec

--- lib/merb_composer/controller_ext.rb
+++ lib/merb_composer/controller_ext.rb
@@ -1,5 +1,9 @@
 class Merb::Controller
 private
   def compose(controller, action, params = {})
+    req = request.dup
+    req.reset_params!
+    req.instance_variable_set :@params, params
+    controller.new(req)._dispatch(action).render :layout => false
   end
 end
